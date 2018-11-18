3d4a185d9ec775be4a5cdef85240421dae1e5678
SlowestLoris <zhangraymond05@gmail.com>
2018-11-18 14:17:04 -0500

    add src

---
+++ src/dmopc18c3p2.java
@@ -0,0 +1,152 @@
+import java.util.*;
+import java.io.*;
+public class dmopc18c3p2 {
+	
+	static int dp[][] = new int[100005][3];
+	static int a[] = new int[100005], b[] = new int[100005], N;
+	
+	static int max(int a, int b, int c) {
+		return a > b && a > c ? a : b > c ? b : c;
+	}
+
+	public static void main(String[] args) throws IOException {
+		N = readInt();
+		for(int i=1;i<=N;i++) a[i] = readInt();
+		for(int i=1;i<=N;i++) b[i] = readInt();
+		for(int i=1;i<=N+1;i++) {
+			dp[i][0] = max(dp[i-1][0],dp[i-1][1],dp[i-1][2]) + b[i];
+			if(i<=N) {
+				dp[i][1] = dp[i-1][0] + a[i];
+				dp[i][2] = dp[i-1][1] + a[i];
+			}
+		}
+		pr.println(dp[N+1][0]);
+		pr.close();
+	}
+
+	final private static int BUFFER_SIZE = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[64]; // line length
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+	public static String read() throws IOException{
+		byte[] ret = new byte[1024];
+        int idx = 0;
+        byte c = Read();
+        while (c <= ' ') {
+            c = Read();
+        }
+        do {
+            ret[idx++] = c;
+            c = Read();
+        } while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+        return new String(ret, 0, idx);
+	}
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
+	}
+}
