188d38dea824d82a6fa09f7da83ebfe7716951d5
SlowestLoris <zhangraymond05@gmail.com>
2018-11-22 11:56:14 -0500

    update stuff

--- src/ccc02j2.java
+++ src/ccc02j2.java
@@ -1,51 +1,151 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class ccc02j2 {
-	public static void main(String[]args) {
 
-		StringBuilder stringBuilder = new StringBuilder();
-		Scanner sc = new Scanner (System.in);
-		String[] conson = {"a","o","e","i","u","y"};
-		int d = 0;
-		int e = 0;
-
-
-		for(int i = 0; i < 101; i++) {
-
-			String Ameri = sc.nextLine();
-			
-			if (Ameri.equals("quit!")) {
-				break;
-			}
-			
-			else if (Ameri.length() <= 4 || Ameri.contains("or") == false) {
-				System.out.println(Ameri);
-			}
-			
-			else if (Ameri.contains("oor")) {
-				System.out.println(Ameri);
-			}
-			
-			else if (Ameri.contains("ort")) {
-				System.out.println(Ameri);
-			}
-			
-			else if (Ameri.length() >= 4) {
-				String[] Ameriword = Ameri.split("");
-				int Length = Ameriword.length;
-				String change = Ameriword[Length-3] + Ameriword[Length-2] + Ameriword[Length-1] ;
-				if (change.endsWith("or") && change.endsWith("oor") == false) {
-					change = Ameriword[Length-3] + "our";
-
-					for (int f = 0; f < (Ameri.length() -3); f ++) {
-						stringBuilder.append(Ameriword[f]);
-					}
-
-					String finalString = stringBuilder.toString();
-					System.out.println(finalString + change);
-					stringBuilder.setLength(0);
+	public static void main(String[] args) throws IOException {
+		String S = read();
+		int t[] = {0,4,7,14,20,24};
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		for(int i=0;i<t.length;i++) {
+			test.add(t[i]);
+		}
+		while(!S.equals("quit!")) {
+			if(S.length()>4) {
+				String sub = S.substring(S.length()-3, S.length());
+				if(!test.contains(sub.charAt(0)-'a') && sub.endsWith("or")) {
+					sub = sub.charAt(0) + "our";
 				}
+				S = S.substring(0,S.length()-3) + sub;
+			}
+			pr.println(S);
+			S = read();
+		}
+		pr.close();
+	}
+
+	final private static int BUFFER_SIZE = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[64]; // line length
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+	public static String read() throws IOException{
+		byte[] ret = new byte[1024];
+        int idx = 0;
+        byte c = Read();
+        while (c <= ' ') {
+            c = Read();
+        }
+        do {
+            ret[idx++] = c;
+            c = Read();
+        } while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+        return new String(ret, 0, idx);
+	}
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
 			}
 		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
 	}
 }
